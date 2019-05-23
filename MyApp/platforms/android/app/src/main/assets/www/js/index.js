e184d582de7366d028740c916e5b379d959e16ad
JonasMaennle <jonas.maennle@gmx.de>
2019-05-23 10:48:04 +0200

    nice changes

--- MyApp/platforms/android/app/src/main/assets/www/js/index.js
+++ MyApp/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,20 +1,5 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Lorem ipsum by Drinking-Support.com
  */
 var app = {
     // Application Constructor
@@ -26,13 +11,40 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
+    bindEvents: function(){
+    	document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        $("#target").bind("tap", app.tapHandler);
+        navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
+    },
+    
+    tapHandler: function(event){
+		alert("geklickt");
     },
 
+	 // onSuccess Geolocation
+    onSuccess: function(position) {
+    	var element = document.getElementById('geolocation');
+		element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+				  'Longitude: '          + position.coords.longitude             + '<br />' +
+				  'Altitude: '           + position.coords.altitude              + '<br />' +
+				  'Accuracy: '           + position.coords.accuracy              + '<br />' +
+				  'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+				  'Heading: '            + position.coords.heading               + '<br />' +
+				  'Speed: '              + position.coords.speed                 + '<br />' +
+				  'Timestamp: '          + position.timestamp                    + '<br />';
+    },
+
+	// onError Callback receives a PositionError object
+	onError: function(error) {
+		alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+	},
+	
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+	receivedEvent: function(id) {
+		var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 

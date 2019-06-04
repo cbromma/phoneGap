fbe665ffac758974976e462a10f9f81c152d9c1d
JonasMaennle <jonas.maennle@gmx.de>
2019-06-04 13:47:17 +0200

    tryed some stuff but not working as it should :(

--- MyApp/platforms/android/app/src/main/assets/www/js/index.js
+++ MyApp/platforms/android/app/src/main/assets/www/js/index.js
@@ -10,12 +10,9 @@ var currentRunObject = [];
 var runDuration = 0;
 var runDistance = 0;
 var runTrigger = false;
-
-
+var picture = 0;
 
 var app = {
-
-
 		
     // Application Constructor
     initialize: function() {
@@ -41,23 +38,23 @@ var app = {
         $("#start").bind("tap", app.mapStarter);
 
     },
-    
-    
 
-    //start the map/run
+    // start button -> start the map/run
     mapStarter: function(event){
 
     	runTrigger = true;
-        var position = {lat: positionLat, lng: positionLang};  
+        var position = 0;  
         
         setInterval(function(){
         	//console.log("test");
         	if(runTrigger){
         		navigator.geolocation.watchPosition(app.onSuccess, app.onError);
-        		position = {lat: positionLat, lng: positionLang};
-                currentRunArray.push(position);
-                runDuration++;
-                console.log(position);
+        		//var position = {lat: positionLat, lng: positionLang};
+        		if(positionLat != 0 && positionLang != 0){
+                    currentRunArray.push({lat: positionLat, lng: positionLang});
+                    runDuration++;
+        		}
+                console.log("Lat: "+ positionLat + " Lng: " + positionLang);
         	}
         }, 1000);
         
@@ -67,38 +64,71 @@ var app = {
         }, 2000);
     },
 
-    // test button -> camera function
+    // stop button -> save object to jason file -> camera function
     tapHandler: function(event){
     	currentRunArray.shift();
     	runTrigger = false;
     	
+    	// do picture
+//		navigator.camera.getPicture(app.onCameraSuccess, app.onCameraFail, { quality: 50,
+//		    destinationType: Camera.DestinationType.DATA_URL
+//		});
     	
-    	
-        
-        // create runObject
+		// calc distance
+		for(var i = 0; i < currentRunArray.length - 2; i++){
+			runDistance += app.calcDistance(currentRunArray[i].positionLat, currentRunArray[i].positionLang, currentRunArray[i + 1].positionLat, currentRunArray[i + 1].positionLang);
+		}
+		// positionLat, lng: positionLang
+		// (lat1, lon1, lat2, lon2)
+		alert(runDistance);
+		
+		// create runObject / marker positions / duration / picture / distance
     	currentRunObject = [currentRunArray, runDuration, null, runDistance];
     	alert(currentRunObject);
     	
     	// write object to file
     	var jsonData = JSON.stringify(currentRunObject);
-        var fs = require('fs');
-    	fs.writeFile("././test.txt", jsonData, function(err) {
-    	    if (err) {
-    	        console.log(err);
-    	    }
-    	});
-    	
 
-		navigator.camera.getPicture(app.onCameraSuccess, app.onCameraFail, { quality: 50,
-		    destinationType: Camera.DestinationType.DATA_URL
-		});
+    	
+    	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+            fileSystem.root.getFile("saved_runs.txt", {create: true, exclusive: false}, function(fileEntry){
+                fileEntry.createWriter(function(writer){
+                    if(writer.length <= 5){
+                        writer.write(jsonData);
+                        alert("Hat gespeichert");
+                    }
+                }, app.onError);
+            }, app.onError);
+        }, null);
+    },
+    
+    calcDistance: function(lat1, lon1, lat2, lon2) {
+    	if ((lat1 == lat2) && (lon1 == lon2)) {
+    		return 0;
+    	}
+    	else {
+    		var radlat1 = Math.PI * lat1/180;
+    		var radlat2 = Math.PI * lat2/180;
+    		var theta = lon1-lon2;
+    		var radtheta = Math.PI * theta/180;
+    		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    		if (dist > 1) {
+    			dist = 1;
+    		}
+    		dist = Math.acos(dist);
+    		dist = dist * 180/Math.PI;
+    		dist = dist * 60 * 1.1515;
+    		dist = dist * 1.609344;
+
+    		return dist;
+    	}
     },
     
     onCameraSuccess: function(imageData) {
         var image = document.getElementById('myImage');
         image.src = "data:image/jpeg;base64," + imageData;
 
-        
+        picture = image.src;
     },
 
     onCameraFail: function(message) {
@@ -136,11 +166,6 @@ var app = {
 
         console.log('Received Event: ' + id);
     },
-	
-	
-	
-
-
 };
 
 app.initialize();
